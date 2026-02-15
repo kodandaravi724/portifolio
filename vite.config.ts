62a8db28e812ded1c52b8dbd4d71210520fe7714
Kodanda Ravi Kiran Putta <kodandaravikiranputta@Greeshmas-MacBook-Air.local>
2026-02-15 14:11:30 -0600

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/portifolio/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
